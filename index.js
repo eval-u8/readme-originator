7f8d793b8a7dedde353aee22e9b3a33b47a0223e
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-04 19:48:16 -0500

    testing func

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -103,10 +105,19 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const genReadMe = generateMarkdown();
+
+function writeToFile(fileName, data) {
+    fs.writeFile('./README.md', genReadMe, err => {
+        if (err) throw err;
+        console.log('README complete! Check out "README.md" to see your file!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(log => console.log(log))
+};
 
 // Function call to initialize app
 init();
