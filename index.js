cc9cbe1d5e08435fe39e6e811490aa74fd92375c
Ernesto Valecillos <evalecillos@gmail.com>
2021-10-04 19:14:48 -0500

    added questions

--- index.js
+++ index.js
@@ -3,7 +3,104 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "project title",
+        message: "What is the title of the project? (Required)",
+        validate: (projectTitle) => {
+            if (projectTitle) {
+                return true;
+            } else {
+                console.log("Please enter the title of your project.");
+                return false;
+            }
+        },
+    },
+    {
+        input: "input",
+        name: "githubUser",
+        message: "What is your GitHub username?",
+        validate: (ghuserInput) => {
+            if (ghuserInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username.");
+                return false;
+            }
+        },
+    },
+    {
+        input: "input",
+        name: "email",
+        message: "What is your e-mail address?",
+        validate: (emailAdd) => {
+            if (emailAdd) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username.");
+                return false;
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of the project (Required)",
+        validate: (projectdInput) => {
+            if (projectdInput) {
+                return true;
+            } else {
+                console.log("Please enter a description for your project.");
+                return false;
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the project's installation instructions.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter the projects usage details.",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message:
+            "Please enter details as to how people can contribute to the project.",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter project test instructions",
+        validate: (testInput) => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log("Please enter test instructions");
+                return false;
+            }
+        },
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which License would you like to use?",
+        choices: [
+            "ISC License",
+            "GNU GPLv3",
+            "zLib License",
+            "Mozilla Public License 2.0",
+            "Apache License 2.0",
+            "MIT License",
+            "Boost Software License 1.0",
+            "WTFPL",
+        ],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
